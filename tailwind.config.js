453c5c91d950f7a06837b15817b803b132a96d84
Adriel Gama <adrielgama@gmail.com>
2023-08-17 20:50:03 +0100

    feat: create under construction page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        wine600: "#1E020D",
+        wine400: "#3D051C",
+        wine100: "#9F6177",
+        gold: "#B29557",
+        "gold-gradient": "#5F4D28",
+        "gray-light": "#d3dce6",
+      },
+      backgroundImage: {
+        'bg-logo': "url(/assets/logo_short_favicon.png)",
+      },
+    },
+  },
+};
