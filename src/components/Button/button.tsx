f8f703e0e8df3d2a9c1f8738b2050d9bdd1f523b
Adriel Gama <adrielgama@gmail.com>
2023-11-26 01:46:41 -0300

    fix: onClick contact button

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -4,8 +4,10 @@ import { Button as BtnComponent } from '../ui/button'
 
 export const Button = () => {
   return (
-    <BtnComponent className="mt-4 max-w-[170px] rounded-full bg-gradient-to-r from-wine400 to-wine600 p-4 text-center text-xs text-white">
-      Entrar em contato
-    </BtnComponent>
+    <a href="#contact">
+      <BtnComponent className="mt-4 max-w-[170px] rounded-full bg-gradient-to-r from-wine400 to-wine600 p-4 text-center text-xs text-white">
+        Entrar em contato
+      </BtnComponent>
+    </a>
   )
 }
