e99df5008b43dda2d5936227ca269f1144a12ea7
Adriel Gama <adrielgama@gmail.com>
2025-03-04 21:59:21 -0300

    feat: add vecel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Inter, Montserrat, Neuton } from 'next/font/google'
 
 import { Toaster } from '@/components/ui/sonner'
@@ -94,6 +95,7 @@ export default function RootLayout({
           {children}
           <Toaster richColors theme="light" />
           <Analytics />
+          <SpeedInsights />
         </ReactQueryProvider>
       </body>
     </html>
